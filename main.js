28967403deb943000f3c26b8e6b1c37ad1eaf8ed
SavlatHasanov <hasanovsavlat53@gmail.com>
2024-04-24 18:55:09 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//console.log(1 + 2)//3
+//console.log(1 - 2)//-1
+//console.log(1 * 2)//2
+//console.log(1 / 2)//0.5
+
+//**,%
+
+//console.log(2**4)//16
+//console.log(4%3)//1
+
+//let a ='45';
+//let b ='45';
+
+//console.log( b + a );//45
+
+
+//const age = prompt('Введите возрост')
+//const isMarried =confirm('вам есть 18')
+
+//const abc = alert(Продолжать)
+//console.log(age)
+//console.log(isMarried)
+
+
